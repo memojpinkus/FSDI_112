c0bcfb74cba9b56c1575a7b629e01eab07beca8c
memojpinkus <memojpinkus@gmail.com>
2022-04-29 19:43:17 -0700

    Added test cases

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_contains_content(self):
+        response = self.client.get("/")
+        self.assertContains(response, "Hello world")
+
+    def test_home_page_uses_correct_templates(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
+        self.assertTemplateUsed(response, "base.html")
+
+    def test_about_page_contains_content(self):
+        response = self.client.get("/about/")
+        self.assertContains(response, "My name is Guillermo Isaac Jimenez Pinkus")
+    
+    def test_about_page_uses_correct_templates(self):
+        response = self.client.get("/about/")
+        self.assertTemplateUsed(response, "about.html")
+        self.assertTemplateUsed(response, "base.html")
+
+    def test_home_page_reverse_lookup(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_about_page_reverse_lookup(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "about.html")
